e86512cd766da3b72dbd79cd815c6f94d0ca5b50
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-12 20:47:11 +0300

    Fixes issue where server did not boot

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -5,4 +5,15 @@ echo "Preparing DB..."
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 echo "DB ready!"
 
+# Rails automatically creates the folder during initialization when
+# you start the server with bundle exec rails server (puma). This
+# folder creation will be skipped if you start the server with
+# bundle exec puma, resulting in the error. It would be nice
+# though if both commands had the same result, saves some
+# errors like these.
+# https://stackoverflow.com/questions/52862529/no-such-file-or-directory-rb-sysopen-tmp-pids-puma-pid
+# Should we start this with "bundle exec rails server" instead?
+
+mkdir -p tmp/pids
+
 bundle exec puma -C config/puma.rb
