8efe0a167df5f9e01a244930531501067a1b6338
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-12 19:35:07 +0300

    No need to do this

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
-
 echo "Preparing DB..."
 # If database exists, migrate. Otherwise, setup
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
