d27a03f5248453afd7ba9c1b8bb16c69066ba25b
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-12 18:56:59 +0300

    Precompiles assets to the image; updates webpack dev server

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -3,12 +3,6 @@
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-if test "$RAILS_ENV" = "production"
-  then
-    echo "Precompiling assets..."
-    bundle exec rake assets:precompile
-fi
-
 echo "Preparing DB..."
 # If database exists, migrate. Otherwise, setup
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
