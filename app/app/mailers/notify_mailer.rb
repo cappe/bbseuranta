cb5bcf260ebea847b62d9e7d954739f373dfd9bb
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-12 14:45:09 +0300

    Refactors

--- app/app/mailers/notify_mailer.rb
+++ app/app/mailers/notify_mailer.rb
@@ -1,4 +1,6 @@
 class NotifyMailer < ApplicationMailer
+  before_action :set_recipient
+  before_action :set_user_link
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -6,16 +8,22 @@ class NotifyMailer < ApplicationMailer
   #   en.notify_mailer.episode_notification.subject
   #
   def episode_notification
-    @recipient = params[:recipient]
-
     mail to: @recipient.email,
          subject: 'Big Brotherissa tapahtuu!'
   end
 
   def confirmation
-    @recipient = params[:recipient]
-
     mail to: @recipient.email,
          subject: 'BB 24/7 seuranta toimii nyt!'
   end
+
+  private
+
+    def set_recipient
+      @recipient = params[:recipient]
+    end
+
+    def set_user_link
+      @user_link = "https://bbseuranta.fi?id=#{@recipient.id}"
+    end
 end
