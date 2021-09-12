7aaf095c348d799555949a86351ef427b9c1365a
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-12 17:25:30 +0300

    Fixes issues with build process

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-
-set -e
+#! /bin/bash
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+echo "Preparing DB..."
+# If database exists, migrate. Otherwise, setup
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+echo "DB ready!"
+
+bundle exec puma -C config/puma.rb
